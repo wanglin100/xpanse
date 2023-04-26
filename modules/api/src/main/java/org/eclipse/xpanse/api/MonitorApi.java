87eadee9bfeb20c5be7ba896228ae6026eb94eb3
WangLiNaruto <wl-naruto@outlook.com>
2023-04-26 09:24:02 +0800

    Monitor Api

--- modules/api/src/main/java/org/eclipse/xpanse/api/MonitorApi.java
+++ modules/api/src/main/java/org/eclipse/xpanse/api/MonitorApi.java
@@ -6,11 +6,13 @@
 package org.eclipse.xpanse.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -28,6 +30,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -82,14 +85,22 @@ public class MonitorApi {
     @GetMapping(value = "/monitor/metric",
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public List<MonitorMetricResponse> getMonitorMetric(MonitorRequestParams monitorRequestParams) {
-        if (Objects.isNull(monitorRequestParams.getId()) || StringUtils.isBlank(
-                monitorRequestParams.getMonitorType())) {
-            log.error("Parameters Invalid,id: {},getMonitorType: {}", monitorRequestParams.getId(),
-                    monitorRequestParams.getMonitorType());
+    public List<MonitorMetricResponse> getMonitorMetric(
+            @Parameter(name = "id", description = "Id of the deploy service")
+            @RequestParam(name = "id", required = true) String id,
+            @Parameter(name = "monitorType", description = "Monitor resource type")
+            @RequestParam(name = "monitorType", required = true) String monitorType) {
+        if (Objects.isNull(id) || StringUtils.isBlank(monitorType)) {
+            log.error("Parameters Invalid,id: {},getMonitorType: {}", id, monitorType);
             throw new IllegalArgumentException("Parameters Invalid");
         }
-        return orchestratorService.getMonitorMetric(monitorRequestParams);
+        MonitorRequestParams monitorRequestParams = new MonitorRequestParams(UUID.fromString(id),
+                monitorType);
+        List<MonitorMetricResponse> monitorMetricResponseList =
+                orchestratorService.getMonitorMetric(monitorRequestParams);
+        log.info(String.format("Get Monitor metric with params %s "
+                + "success.", monitorRequestParams));
+        return monitorMetricResponseList;
     }
 
     private Map<String, List<ServiceVo>> getServiceInfoMapGroupByName() {
